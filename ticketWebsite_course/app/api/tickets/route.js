d96a15d302087f4e8b1874c5665bc31b576140c6
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-08-27 13:09:04 -0400

    insert data and store the single record

--- ticketWebsite_course/app/api/tickets/route.js
+++ ticketWebsite_course/app/api/tickets/route.js
@@ -5,13 +5,10 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 export async function POST(request) {
   const ticket = await request.json()
 
-  // get supabase instance
   const supabase = createRouteHandlerClient({ cookies })
 
-  // get current user session
   const { data: { session } } = await supabase.auth.getSession()
 
-  // insert the data
   const { data, error } = await supabase.from('tickets')
     .insert({
       ...ticket,
