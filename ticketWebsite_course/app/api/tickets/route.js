38719f1256714fedeeec60f7e2db7ea61b10023c
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-08-27 13:09:04 -0400

    fetching single records

--- ticketWebsite_course/app/api/tickets/route.js
+++ ticketWebsite_course/app/api/tickets/route.js
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 
 export async function POST(request) {
   const ticket = await request.json()
@@ -12,13 +12,6 @@ export async function POST(request) {
   const { data: { session } } = await supabase.auth.getSession()
 
   // insert the data
-  const { data, error } = await supabase.from('tickets')
-    .insert({
-      ...ticket,
-      user_email: session.user.email,
-    })
-    .select()
-    .single()
-
+  
   return NextResponse.json({ data, error })
 }
