15467cc23c0701c807e547196cf7bf3e6bf0c45f
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-08-27 13:09:04 -0400

    insert data and store the single record

--- ticketWebsite_course/app/api/tickets/route.js
+++ ticketWebsite_course/app/api/tickets/route.js
@@ -12,6 +12,13 @@ export async function POST(request) {
   const { data: { session } } = await supabase.auth.getSession()
 
   // insert the data
-  
+  const { data, error } = await supabase.from('tickets')
+    .insert({
+      ...ticket,
+      user_email: session.user.email,
+    })
+    .select()
+    .single()
+
   return NextResponse.json({ data, error })
 }
